de31b8c2fdef8152652232c22104efe6fafc5da8
MatKozyra52 <matkozyra52@gmail.com>
2019-10-23 00:36:39 +0200

    Create sieczne2.m

---
+++ 4/sieczne2.m
@@ -0,0 +1,40 @@
+clear all; close all;
+a=[2, 0, -2];
+x= -2:0.1:3;
+y=polyval (a,x);
+%plot(x,y);
+promien = 5;
+punkt = 0;
+dokladnosc = 0.00001;
+clear x y;
+
+
+x1=punkt+rand*2*promien-promien;
+x2=punkt+rand*2*promien-promien;
+y1=polyval(a,x1);
+y2=polyval(a,x2);
+
+while (sign(y1)==sign(y2))
+    x1=punkt+rand*2*promien-promien;
+    x2=punkt+rand*2*promien-promien;
+    y1=polyval(a,x1);
+    y2=polyval(a,x2);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+xstare = x1;
+xtymczasowe = x2;
+
+while abs(xstare-xtymczasowe)> dokladnosc
+    ystare=polyval(a,xstare);
+    ytymczasowe=polyval(a,xtymczasowe);
+    xnowe = xstare-(ystare*(xtymczasowe-xstare))/(ytymczasowe-ystare);
+
+    xstare = xtymczasowe;
+    xtymczasowe = xnowe;
+    
+end
+
+clear x1 x2 xnowe y1 y2
+disp("Zero w");
+disp(xstare);
