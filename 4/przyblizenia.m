fc3aa3bec0b683b23e09a895fa38d211c02f9b40
MatKozyra52 <matkozyra52@gmail.com>
2019-10-24 00:30:25 +0200

    Update przyblizenia.m

--- 4/przyblizenia.m
+++ 4/przyblizenia.m
@@ -1,8 +1,8 @@
 clear all;
-a=[2,0, -4];
-apoch = wiel_poch(a);
+a=[-2, 6, 9, 0, -1];
 punkt= 0;
-promien= 2;
+promien= 5;
+dokladnosc = 0.00001;
 
 while true
     x1=rand*promien+punkt;
@@ -11,7 +11,7 @@ while true
     y2=polyval(a,x2);
     dx=linspace( x1, x2, 1000);
 
-    k=rand*4-2   ;
+    k=rand*2-1;
     alambda=-k*a;
     alambda(end-1)=alambda(end-1)+1;
     ypochlambda=polyval(wiel_poch(alambda), dx); 
@@ -26,7 +26,12 @@ end
 razy = abs(x1-x2);
 x0=min([x1, x2])+rand*razy;
 
-for i=1:10
+while 1
     y0=polyval(a, x0);
     x0=x0-k*y0;
+    if abs(y0)<dokladnosc
+        break
+    end
 end
+disp("Zero");
+disp(x0);
