6b1cab676e82d41f1b76c32064b92ef4e1c89a18
MatKozyra52 <matkozyra52@gmail.com>
2019-10-24 00:41:38 +0200

    Update Newton_Raphson.m

--- 4/Newton_Raphson.m
+++ 4/Newton_Raphson.m
@@ -1,26 +1,22 @@
 clear all; close all;
 a=[2, 0, -2];
-x= -2:0.1:3;
-y=polyval (a,x);
-%plot(x,y);
 promien = 5;
 punkt = 0;
-clear x y;
 iteracje = 100;
 
-x1=punkt+rand*2*promien-promien;
-x2=punkt+rand*2*promien-promien;
-y1=polyval(a,x1);
-y2=polyval(a,x2);
-a2poch = wiel_poch(wiel_poch(a));
-y2poch = polyval(a2poch, linspace(x1,x2,1000));
 
-while (sign(y1)==sign(y2)) && sign(max(y2poch))==sign(min(y2poch))
+
+while 1
     x1=punkt+rand*2*promien-promien;
     x2=punkt+rand*2*promien-promien;
+    a2poch = wiel_poch(wiel_poch(a));
+    y2poch = polyval(a2poch, linspace(x1,x2,1000));
     y1=polyval(a,x1);
     y2=polyval(a,x2);
     y2poch = polyval(a2poch, linspace(x1,x2,1000));
+    if (sign(y1)~=sign(y2)) && sign(max(y2poch))==sign(min(y2poch))
+        break
+    end
 end
 
 y12poch=polyval(wiel_poch(wiel_poch(a)),x1);
