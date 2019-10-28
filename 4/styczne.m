6bb805f8b06fe94545b1e20d3d64372d01cd3299
MatKozyra52 <matkozyra52@gmail.com>
2019-10-29 00:58:38 +0100

    sr

--- 4/styczne.m
+++ 4/styczne.m
@@ -3,9 +3,37 @@ function [x1, x2, iteracje] = styczne(x1,x2,a,dokladnosc)
     a2poch = wiel_poch(wiel_poch(a));
     y2poch = polyval(a2poch, linspace(x1,x2,1000));
     
-    if (sign(polyval(x1,a))~=sign(polyval(x2,a))) && sign(max(y2poch))==sign(min(y2poch))
+    if (sign(polyval(a,x1))==sign(polyval(a,x2))) && (sign(max(y2poch))~=sign(min(y2poch)))
         disp("nieprawidlowy przedzial");
         return
     end
+    
+    y1=polyval(a,x1);
+    y2=polyval(a,x2);
+    y12poch=polyval(wiel_poch(wiel_poch(a)),x1);
+    y22poch=polyval(wiel_poch(wiel_poch(a)),x2);
+    y11poch=polyval(wiel_poch(a),x1);
+    y21poch=polyval(wiel_poch(a),x2);
+
+    if y1*y12poch > 0
+        x1=x1-y1/y11poch;
+        while abs(x1-x2)>dokladnosc
+            iteracje=iteracje+1;
+            y1=polyval(a,x1);
+            y11poch=polyval(wiel_poch(a),x1);
+            x2=x1;
+            x1=x1-y1/y11poch;
+        end
+    elseif y2*y22poch>0
+        x2=x2-y2/y21poch;
+        while abs(x1-x2)>dokladnosc
+            iteracje=iteracje+1;
+            y2=polyval(a,x2);
+            y21poch=polyval(wiel_poch(a),x2);
+            x1=x2;
+            x2=x2-y2/y21poch;
+        end
+    end
+
 end
 
--- 4/test.m
+++ 4/test.m
@@ -2,5 +2,13 @@ clear all;
 a = [1, 0, -2];
 xa= 0;
 xb= 2;
-dokladnosc = 0.001;
-[x1, x2, iteracje] = bisekcja(xa, xb, a, dokladnosc);
+dokladnosc=linspace(0.00001, 1, 100);
+
+for n=1:100
+[x1, x2, iteracje(n)] = bisekcja(xa, xb, a, dokladnosc(n));
+[x3, x4, iteracje2(n)] = styczne(xa, xb, a, dokladnosc(n));
+end
+
+plot(dokladnosc, iteracje);
+hold on;
+plot(dokladnosc, iteracje2);
